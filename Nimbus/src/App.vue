945043a389ebb40e0f27467dcd1a02dc4ca3dd68
Sophi <195988204+Sophiwo@users.noreply.github.com>
2025-06-05 14:45:38 +0200

    removing this container. works better

--- Nimbus/src/App.vue
+++ Nimbus/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="min-h-screen min-w-screen flex items-center justify-center">
-    <div class="w-full max-w-2xl mx-auto bg-transparent flex flex-col items-center rounded-lg shadow-lg p-2 sm:p-4">
-      <h1 class="text-3xl sm:text-4xl font-bold text-gray-800 mb-4 sm:mb-6 text-center">Nimbus Weather</h1>
+  <div class="w-full min-w-screen max-w-2xl mx-auto bg-transparent flex flex-col items-center rounded-lg shadow-lg p-2 sm:p-4">
+    <h1 class="text-3xl sm:text-4xl font-bold text-gray-800 mb-4 sm:mb-6 text-center">Nimbus Weather</h1>
       <CurrentWeather />
       <ComingHours />
       <ComingDays />
-    </div>
   </div>
 </template>
 
