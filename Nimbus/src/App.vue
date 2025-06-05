ec3c757dbc0793d266bdb9ebf3337ccd640b122f
Sophi <195988204+Sophiwo@users.noreply.github.com>
2025-06-05 14:06:00 +0200

    centering the main ui stuff again

--- Nimbus/src/App.vue
+++ Nimbus/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center">
+  <div class="min-h-screen min-w-screen flex items-center justify-center">
     <div class="w-full max-w-2xl mx-auto bg-transparent flex flex-col items-center rounded-lg shadow-lg p-2 sm:p-4">
       <h1 class="text-3xl sm:text-4xl font-bold text-gray-800 mb-4 sm:mb-6 text-center">Nimbus Weather</h1>
       <CurrentWeather />
