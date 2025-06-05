3ab72b378b9afb54cdac0cb6b4cadee58a35f465
RunaLea <runalea@outlook.com>
2025-06-05 16:23:40 +0200

    Updated useWeatherIcon in AppData

--- Nimbus/src/composables/AppData.ts
+++ Nimbus/src/composables/AppData.ts
@@ -4,23 +4,39 @@ import { useFormatHour } from "./HoursData";
 const isDay = ref(true);
 
 export function useIsDay() { return isDay }
+
 export function useWeatherIcon(code: number, hour: string | null) {
   let src = "";
+  const dayUrl = `/src/assets/tomorrow-icons/${code}0.png`
+  const nightUrl = `/src/assets/tomorrow-icons/${code}1.png`
 
   if (hour) {
     const hourValue = useFormatHour(hour);
     
-    if (Number(hourValue[0] + hourValue[1]) >= 18) {
-      src = `/src/assets/tomorrow-icons/${code}1.png`;
-      return src;
-    }
+    if (Number(hourValue[0] + hourValue[1]) >= 18) { 
+    const img = new Image();
+
+    img.src = nightUrl;
+    if (img.height != 0) { src = nightUrl; }
+    else { src = dayUrl }
+    return src;
+  }
+
     if(Number(hourValue[0] + hourValue[1]) < 18) {
-      src = `/src/assets/tomorrow-icons/${code}0.png`;
+      src = dayUrl;
       return src;
     }
   }
 
-  if (isDay.value) { src = `/src/assets/tomorrow-icons/${code}0.png`; }
-  if (!isDay.value) { src = `/src/assets/tomorrow-icons/${code}1.png`; }
+  if (isDay.value) { src = dayUrl; }
+
+  if (!isDay.value) { 
+    const img = new Image();
+
+    img.src = nightUrl;
+    if (img.height != 0) { src = nightUrl; }
+    else { src = dayUrl }
+  }
+
   return src;
 }
