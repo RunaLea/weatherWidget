8d1d26b1675e8f132bfebadd3549f0be7d4d846d
RunaLea <runalea@outlook.com>
2025-06-06 15:55:31 +0200

    Changed Favicon and page title

--- Nimbus/src/App.vue
+++ Nimbus/src/App.vue
@@ -14,11 +14,23 @@ import ComingDays from './components/ComingDays.vue';
 import { useIsDay } from './composables/AppData'
 import { watchEffect } from 'vue'
 import { useCurrentWeather } from './composables/CurrentData';
+import { useWeatherLabel } from './composables/CurrentData';
+import { useWeatherIcon } from './composables/AppData';
 
 const currentWeather = useCurrentWeather();
 const isDay = useIsDay(currentWeather.time);
+const weatherLabel = useWeatherLabel();
 
 watchEffect(() => {
   document.body.classList.toggle('night-bg', !isDay);
+  document.title = `Nimbus - ${weatherLabel.value}`;
+  const url = useWeatherIcon(currentWeather.values.weatherCode, currentWeather.time);
+  let link: HTMLLinkElement | null = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.href = url || '';
 });
 </script>
