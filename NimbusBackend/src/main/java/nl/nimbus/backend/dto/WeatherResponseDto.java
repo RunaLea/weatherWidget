2d6bf33645f8f839129f37b78b58a7904f92b8f2
emil <emilvdijk@gmail.com>
2025-06-09 14:50:55 +0200

    updated dto

--- NimbusBackend/src/main/java/nl/nimbus/backend/dto/WeatherResponseDto.java
+++ NimbusBackend/src/main/java/nl/nimbus/backend/dto/WeatherResponseDto.java
@@ -1,167 +1,186 @@
 package nl.nimbus.backend.dto;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class WeatherResponseDto {
 
-  public class Timelines{
+  public class Timelines {
     public ArrayList<Minutely> minutely;
     public ArrayList<Hourly> hourly;
     public ArrayList<Daily> daily;
 
-    public static class Minutely{
-      public Date time;
+    public static class Minutely {
+      public Instant  time;
       public Values values;
+
+      public static class Values {
+        public Double cloudBase;
+        public Double cloudCeiling;
+        public Integer cloudCover;
+        public Double dewPoint;
+        public Integer freezingRainIntensity;
+        public Integer humidity;
+        public Integer precipitationProbability;
+        public Double pressureSeaLevel;
+        public Double pressureSurfaceLevel;
+        public Double rainIntensity;
+        public Integer sleetIntensity;
+        public Integer snowIntensity;
+        public Double temperature;
+        public Double temperatureApparent;
+        public Integer uvHealthConcern;
+        public Integer uvIndex;
+        public Integer visibility;
+        public Integer weatherCode;
+        public Integer windDirection;
+        public Double windGust;
+        public Double windSpeed;
+      }
     }
 
-      public static class Hourly{
-        public Date time;
-        public Values values;
-      }
+    public static class Hourly {
+      public Instant time;
+      public Values values;
 
-      public static class Daily{
-        public Date time;
-        public Values values;
+      public static class Values {
+        public Double cloudBase;
+        public Double cloudCeiling;
+        public Integer cloudCover;
+        public Double dewPoint;
+        public Integer freezingRainIntensity;
+        public Integer humidity;
+        public Integer iceAccumulation;
+        public Integer iceAccumulationLwe;
+        public Integer precipitationProbability;
+        public Double pressureSeaLevel;
+        public Double pressureSurfaceLevel;
+        public Double rainAccumulation;
+        public Double rainIntensity;
+        public Integer sleetAccumulation;
+        public Integer sleetAccumulationLwe;
+        public Integer sleetIntensity;
+        public Integer snowAccumulation;
+        public Integer snowAccumulationLwe;
+        public Integer snowIntensity;
+        public Double temperature;
+        public Double temperatureApparent;
+        public Integer uvHealthConcern;
+        public Integer uvIndex;
+        public Double visibility;
+        public Integer weatherCode;
+        public Integer windDirection;
+        public Double windGust;
+        public Double windSpeed;
       }
-
-    public static class Values{
-      public double cloudBase;
-      public double cloudCeiling;
-      public int cloudCover;
-      public double dewPoint;
-      public int freezingRainIntensity;
-      public int humidity;
-      public int precipitationProbability;
-      public double pressureSeaLevel;
-      public double pressureSurfaceLevel;
-      public int rainIntensity;
-      public int sleetIntensity;
-      public int snowIntensity;
-      public double temperature;
-      public double temperatureApparent;
-      public int uvHealthConcern;
-      public int uvIndex;
-      public double visibility;
-      public int weatherCode;
-      public int windDirection;
-      public double windGust;
-      public double windSpeed;
-      public int iceAccumulation;
-      public int iceAccumulationLwe;
-      public int rainAccumulation;
-      public int sleetAccumulation;
-      public int sleetAccumulationLwe;
-      public int snowAccumulation;
-      public int snowAccumulationLwe;
-      public double cloudBaseAvg;
-      public double cloudBaseMax;
-      public double cloudBaseMin;
-      public double cloudCeilingAvg;
-      public double cloudCeilingMax;
-      public int cloudCeilingMin;
-      public int cloudCoverAvg;
-      public int cloudCoverMax;
-      public int cloudCoverMin;
-      public double dewPointAvg;
-      public double dewPointMax;
-      public double dewPointMin;
-      public int freezingRainIntensityAvg;
-      public int freezingRainIntensityMax;
-      public int freezingRainIntensityMin;
-      public int humidityAvg;
-      public int humidityMax;
-      public int humidityMin;
-      public int iceAccumulationAvg;
-      public int iceAccumulationLweAvg;
-      public int iceAccumulationLweMax;
-      public int iceAccumulationLweMin;
-      public int iceAccumulationLweSum;
-      public int iceAccumulationMax;
-      public int iceAccumulationMin;
-      public int iceAccumulationSum;
-      public Date moonriseTime;
-      public Date moonsetTime;
-      public double precipitationProbabilityAvg;
-      public int precipitationProbabilityMax;
-      public int precipitationProbabilityMin;
-      public double pressureSeaLevelAvg;
-      public double pressureSeaLevelMax;
-      public double pressureSeaLevelMin;
-      public double pressureSurfaceLevelAvg;
-      public double pressureSurfaceLevelMax;
-      public double pressureSurfaceLevelMin;
-      public double rainAccumulationAvg;
-      public double rainAccumulationMax;
-      public int rainAccumulationMin;
-      public double rainAccumulationSum;
-      public double rainIntensityAvg;
-      public double rainIntensityMax;
-      public int rainIntensityMin;
-      public int sleetAccumulationAvg;
-      public int sleetAccumulationLweAvg;
-      public int sleetAccumulationLweMax;
-      public int sleetAccumulationLweMin;
-      public int sleetAccumulationLweSum;
-      public int sleetAccumulationMax;
-      public int sleetAccumulationMin;
-      public int sleetIntensityAvg;
-      public int sleetIntensityMax;
-      public int sleetIntensityMin;
-      public int snowAccumulationAvg;
-      public int snowAccumulationLweAvg;
-      public int snowAccumulationLweMax;
-      public int snowAccumulationLweMin;
-      public int snowAccumulationLweSum;
-      public int snowAccumulationMax;
-      public int snowAccumulationMin;
-      public int snowAccumulationSum;
-      public int snowIntensityAvg;
-      public int snowIntensityMax;
-      public int snowIntensityMin;
-      public Date sunriseTime;
-      public Date sunsetTime;
-      public double temperatureApparentAvg;
-      public double temperatureApparentMax;
-      public double temperatureApparentMin;
-      public double temperatureAvg;
-      public double temperatureMax;
-      public double temperatureMin;
-      public int uvHealthConcernAvg;
-      public int uvHealthConcernMax;
-      public int uvHealthConcernMin;
-      public int uvIndexAvg;
-      public int uvIndexMax;
-      public int uvIndexMin;
-      public double visibilityAvg;
-      public int visibilityMax;
-      public double visibilityMin;
-      public int weatherCodeMax;
-      public int weatherCodeMin;
-      public int windDirectionAvg;
-      public double windGustAvg;
-      public double windGustMax;
-      public double windGustMin;
-      public double windSpeedAvg;
-      public double windSpeedMax;
-      public double windSpeedMin;
     }
 
+    public static class Daily {
+      public Instant  time;
+      public Values values;
 
+      public static class Values {
+        public Double cloudBaseAvg;
+        public Double cloudBaseMax;
+        public Double cloudBaseMin;
+        public Double cloudCeilingAvg;
+        public Double cloudCeilingMax;
+        public Integer cloudCeilingMin;
+        public Integer cloudCoverAvg;
+        public Integer cloudCoverMax;
+        public Integer cloudCoverMin;
+        public Double dewPointAvg;
+        public Double dewPointMax;
+        public Double dewPointMin;
+        public Integer freezingRainIntensityAvg;
+        public Integer freezingRainIntensityMax;
+        public Integer freezingRainIntensityMin;
+        public Integer humidityAvg;
+        public Integer humidityMax;
+        public Integer humidityMin;
+        public Integer iceAccumulationAvg;
+        public Integer iceAccumulationLweAvg;
+        public Integer iceAccumulationLweMax;
+        public Integer iceAccumulationLweMin;
+        public Integer iceAccumulationLweSum;
+        public Integer iceAccumulationMax;
+        public Integer iceAccumulationMin;
+        public Integer iceAccumulationSum;
+        public Date moonriseTime;
+        public Date moonsetTime;
+        public Integer precipitationProbabilityAvg;
+        public Integer precipitationProbabilityMax;
+        public Integer precipitationProbabilityMin;
+        public Double pressureSeaLevelAvg;
+        public Double pressureSeaLevelMax;
+        public Double pressureSeaLevelMin;
+        public Double pressureSurfaceLevelAvg;
+        public Double pressureSurfaceLevelMax;
+        public Double pressureSurfaceLevelMin;
+        public Integer rainAccumulationAvg;
+        public Integer rainAccumulationMax;
+        public Integer rainAccumulationMin;
+        public Integer rainAccumulationSum;
+        public Double rainIntensityAvg;
+        public Double rainIntensityMax;
+        public Integer rainIntensityMin;
+        public Integer sleetAccumulationAvg;
+        public Integer sleetAccumulationLweAvg;
+        public Integer sleetAccumulationLweMax;
+        public Integer sleetAccumulationLweMin;
+        public Integer sleetAccumulationLweSum;
+        public Integer sleetAccumulationMax;
+        public Integer sleetAccumulationMin;
+        public Integer sleetIntensityAvg;
+        public Integer sleetIntensityMax;
+        public Integer sleetIntensityMin;
+        public Integer snowAccumulationAvg;
+        public Integer snowAccumulationLweAvg;
+        public Integer snowAccumulationLweMax;
+        public Integer snowAccumulationLweMin;
+        public Integer snowAccumulationLweSum;
+        public Integer snowAccumulationMax;
+        public Integer snowAccumulationMin;
+        public Integer snowAccumulationSum;
+        public Integer snowIntensityAvg;
+        public Integer snowIntensityMax;
+        public Integer snowIntensityMin;
+        public Date sunriseTime;
+        public Date sunsetTime;
+        public Double temperatureApparentAvg;
+        public Double temperatureApparentMax;
+        public Double temperatureApparentMin;
+        public Double temperatureAvg;
+        public Double temperatureMax;
+        public Double temperatureMin;
+        public Integer uvHealthConcernAvg;
+        public Integer uvHealthConcernMax;
+        public Integer uvHealthConcernMin;
+        public Integer uvIndexAvg;
+        public Integer uvIndexMax;
+        public Integer uvIndexMin;
+        public Double visibilityAvg;
+        public Integer visibilityMax;
+        public Double visibilityMin;
+        public Integer weatherCodeMax;
+        public Integer weatherCodeMin;
+        public Integer windDirectionAvg;
+        public Double windGustAvg;
+        public Double windGustMax;
+        public Double windGustMin;
+        public Double windSpeedAvg;
+        public Double windSpeedMax;
+        public Double windSpeedMin;
+      }
+    }
   }
-
-
 
   public Timelines timelines;
   public Location location;
 
-  public static class Location{
-    public double lat;
-    public double lon;
+  public static class Location {
+    public Double lat;
+    public Double lon;
   }
-
 }
-
-
-
-
