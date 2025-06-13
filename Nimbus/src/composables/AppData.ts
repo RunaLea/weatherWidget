629c69d15f8e2ba9d50db42d8a47b4dfce970964
RunaLea <runalea@outlook.com>
2025-06-13 13:00:43 +0200

    Fixed useWeatherIcon, night icons should display in Next Hours now

--- Nimbus/src/composables/AppData.ts
+++ Nimbus/src/composables/AppData.ts
@@ -20,46 +20,24 @@ export function isLoading() {return loading}
 export function useIsDay(hour: string) {
   const hourValue = useFormatHour(hour);
   const hourNum = Number(hourValue[0] + hourValue[1])
-// FIXME doesnt work :(
-  if (hourNum >= 21 || hourNum < 6) {
-    return false
-  }
-  if (hourNum < 21 && hourNum >= 6) {
-    return true
-  }
+
+  if (hourNum >= 21 || hourNum < 6) { return false }
+  if (hourNum < 21 && hourNum >= 6) { return true }
 }
 
 export function useWeatherIcon(code: number, hour: string | null) {
   const dayUrl = `/src/assets/tomorrow-icons/${code}0.png`
   const nightUrl = `/src/assets/tomorrow-icons/${code}1.png`
 
-  if (!isImage(nightUrl)) {
-    return dayUrl
-  }
-
   if (hour) {
-    if (!useIsDay(hour)) {
-      return nightUrl
-    }
-    if (useIsDay(hour)) {
-      return dayUrl
-    }
+    if (!useIsDay(hour)) { return nightUrl }
+    if (useIsDay(hour)) { return dayUrl }
   }
 
-  if (useIsDay(useWeather().value.timelines.hourly[0].time)) {
-    return dayUrl
-  }
-  if (!useIsDay(useWeather().value.timelines.hourly[0].time)) {
-    return nightUrl
-  }
+  if (useIsDay(useWeather().value.timelines.hourly[0].time)) { return dayUrl }
+  if (!useIsDay(useWeather().value.timelines.hourly[0].time)) { return nightUrl }
 }
 
-export function isImage(url: string) {
-  const img = new Image();
-  img.src = url
-  if (img.height != 0) {
-    return true;
-  } else {
-    return false
-  }
+export function useDayIcon(code: number) {
+  return `/src/assets/tomorrow-icons/${code}0.png`
 }
